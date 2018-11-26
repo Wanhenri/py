686e7e2ec965d03bc6e503621543708065c53a49
jk-active-wan_-henri <slipkorn.jk@gmail.com>
2018-11-26 15:01:21 -0200

    Create simple-bar-chart.py

---
+++ projeto_12/run/simple-bar-chart.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot as plt
+
+drinks = ["cappuccino", "latte", "chai", "americano", "mocha", "espresso"]
+sales =  [91, 76, 56, 66, 52, 27]
+
+plt.bar(range(len(sales)), sales)
+plt.show()
+plt.savefig('SBC.png')
+
+plt.close('all')
+
+
+plt.bar(range(len(drinks)), sales)
+
+#create your ax object here
+ax = plt.subplot()
+ax.set_xticks(range(len(drinks)))
+ax.set_xticklabels(drinks)
+plt.savefig('drinks.png')
+plt.show()
