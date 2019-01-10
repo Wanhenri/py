caf3705d8f0538e308d458f85ff9cb7f5cbd1023
Wanderson Henrique <slipkorn.jk@gmail.com>
2019-01-10 09:50:24 -0200

    Create Stacked_Bars.py

---
+++ projeto_17/Stacked_Bars.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+
+drinks = ["cappuccino", "latte", "chai", "americano", "mocha", "espresso"]
+sales1 =  [91, 76, 56, 66, 52, 27]
+sales2 = [65, 82, 36, 68, 38, 40]
+  
+plt.bar(range(len(drinks)), sales1)
+plt.bar(range(len(drinks)), sales2, bottom=sales1)
+
+plt.legend(["sales 1", "sales 2"])
+
+plt.show()
